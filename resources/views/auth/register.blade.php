3a2fb77a70ecff9b4903aa962836811460094efe
giovannispaziani <giovanni.spaziani@student.univaq.it>
2022-05-24 12:47:46 +0200

    Modifica vista di registrazione

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,7 +11,7 @@
           <div class="card-header card-header-primary text-center">
             <h4 class="card-title"><strong>{{ __('Register') }}</strong></h4>
             <div class="social-line">
-              <a href="#pablo" class="btn btn-just-icon btn-link btn-white">
+              <!--<a href="#pablo" class="btn btn-just-icon btn-link btn-white">
                 <i class="fa fa-facebook-square"></i>
               </a>
               <a href="#pablo" class="btn btn-just-icon btn-link btn-white">
@@ -19,11 +19,12 @@
               </a>
               <a href="#pablo" class="btn btn-just-icon btn-link btn-white">
                 <i class="fa fa-google-plus"></i>
-              </a>
+              </a>-->
             </div>
           </div>
           <div class="card-body ">
-            <p class="card-description text-center">{{ __('Or Be Classical') }}</p>
+            <!--<p class="card-description text-center">{{ __('Or Be Classical') }}</p>-->
+            <!--name-->
             <div class="bmd-form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
               <div class="input-group">
                 <div class="input-group-prepend">
@@ -39,6 +40,23 @@
                 </div>
               @endif
             </div>
+            <!--Surname-->
+            <div class="bmd-form-group{{ $errors->has('surname') ? ' has-danger' : '' }}" style="margin-top: 5%">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                      <i class="material-icons">face</i>
+                  </span>
+                </div>
+                <input type="text" name="surname" class="form-control" placeholder="{{ __('Surname...') }}" value="{{ old('surname') }}" required>
+              </div>
+              @if ($errors->has('surname'))
+                <div id="name-error" class="error text-danger pl-3" for="surname" style="display: block;">
+                  <strong>{{ $errors->first('surname') }}</strong>
+                </div>
+              @endif
+            </div>
+            <!--email-->
             <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }} mt-3">
               <div class="input-group">
                 <div class="input-group-prepend">
@@ -54,6 +72,7 @@
                 </div>
               @endif
             </div>
+            <!--password-->
             <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
               <div class="input-group">
                 <div class="input-group-prepend">
@@ -69,6 +88,7 @@
                 </div>
               @endif
             </div>
+            <!--passconfirm-->
             <div class="bmd-form-group{{ $errors->has('password_confirmation') ? ' has-danger' : '' }} mt-3">
               <div class="input-group">
                 <div class="input-group-prepend">
@@ -84,8 +104,28 @@
                 </div>
               @endif
             </div>
+            
+              <!-- Example single danger button -->
+              <div class="form-check form-check-radio" style="margin-left: 5% ; margin-top: 10%">
+                <label class="form-check-label">
+                <input class="form-check-input" type="radio" name="type" id="exampleRadios1" value="Ricercatore" checked>
+                Ricercatore
+                <span class="circle">
+                  <span class="check"></span>
+                </span>
+                </label>
+              
+                <label class="form-check-label" style="margin-left:20px">
+                <input class="form-check-input" type="radio" name="type" id="exampleRadios2" value="Manager">
+                  Manager
+                  <span class="circle">
+                   <span class="check"></span>
+                </span>
+                  </label>
+              </div>
+             
             <div class="form-check mr-auto ml-3 mt-3">
-              <label class="form-check-label">
+              <label class="form-check-label" style="margin-top:10%">
                 <input class="form-check-input" type="checkbox" id="policy" name="policy" {{ old('policy', 1) ? 'checked' : '' }} >
                 <span class="form-check-sign">
                   <span class="check"></span>
