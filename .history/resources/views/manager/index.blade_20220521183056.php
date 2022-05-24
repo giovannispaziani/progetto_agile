bfb61deb52f9af2b1ae6dd6391f4c5474c3468a2
longobucco <luca.visconti2@student.univaq.it>
2022-05-24 13:03:40 +0200

    guest edit

---
+++ .history/resources/views/manager/index.blade_20220521183056.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Emplooyes Lists</title>
+</head>
+
+<body>
+	<h1>Employees</h1>
+</body>
+</html>
---
+++ .history/resources/views/manager/index.blade_20220521184357.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Manager Dashboard</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-primary">
+    <div class="container">
+        <p>Bentornato</p>
+      <!-- <a class="navbar-brand" href="#">Bentornato</a><-->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-bar navbar-kebab"></span>
+      <span class="navbar-toggler-bar navbar-kebab"></span>
+      <span class="navbar-toggler-bar navbar-kebab"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="login">Log In</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="register">Sign Up</a>
+          </li>
+        </ul>
+        <form class="form-inline ml-auto">
+            <div class="form-group no-border">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-neutral btn-icon btn-round">
+                <i class="material-icons">search</i>
+            </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+</body>
+
+</html>
