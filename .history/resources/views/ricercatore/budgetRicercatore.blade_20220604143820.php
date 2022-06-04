1a9f4f35ae885edd47a655ff0b28e0281bafb366
longobucco <luca.visconti2@student.univaq.it>
2022-06-04 16:45:58 +0200

    Interfaccia pubblicazioni + edit Ricercatore

---
+++ .history/resources/views/ricercatore/budgetRicercatore.blade_20220604143820.php
@@ -0,0 +1,90 @@
+@extends('layouts.ricercatore.appRicercatore', ['activePage' => 'table', 'titlePage' => __('Table List')])
+
+@section('content')
+<div class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header card-header-primary">
+            <h4 class="card-title ">Richieste budget</h4>
+            <p class="card-category"></p>
+          </div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table">
+                <thead class=" text-primary">
+                  <th>
+                    ID
+                  </th>
+                  <th>
+                    Progetto
+                  </th>
+                  <th>
+                    Motivo
+                  </th>
+                  <th>
+                    Stato
+                  </th>
+                  <th>
+                    Importo
+                  </th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      3
+                    </td>
+                    <td>
+                      Progetto #1
+                    </td>
+                    <td>
+                      Tools
+                    </td>
+                    <td>
+                      In attesa
+                    </td>
+                    <td class="text-primary">
+                      €500
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      2
+                    </td>
+                    <td>
+                        Progetto #1
+                    </td>
+                    <td>
+                      Curaçao
+                    </td>
+                    <td>
+                      Licenza
+                    </td>
+                    <td class="text-primary">
+                      €350
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      1
+                    </td>
+                    <td>
+                        Progetto #1
+                    </td>
+                    <td>
+                      Netherlands
+                    </td>
+                    <td>
+                      Rifiutato
+                    </td>
+                    <td class="text-primary">
+                      €1000
+                    </td>
+                  </tr>
+                  <tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+@endsection
---
+++ .history/resources/views/ricercatore/sidebarRicercatore.blade_20220604161525.php
@@ -0,0 +1,84 @@
+<div class="sidebar" data-color="orange" data-background-color="white" data-image="{{ asset('material') }}/img/sidebar-1.jpg">
+  <!--
+      Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+      Tip 2: you can also add an image using data-image tag
+  -->
+  <div class="logo">
+    <a href="http://127.0.0.1:8000/ricercatore" class="simple-text logo-normal">
+      {{ __('Stark Industries') }}
+    </a>
+  </div>
+  <div class="sidebar-wrapper">
+    <ul class="nav">
+      <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('dashboardRicercatore') }}">
+          <i class="material-icons">dashboard</i>
+            <p>{{ __('Dashboard') }}</p>
+        </a>
+      </li>
+      <li class="nav-item{{ $activePage == 'profileRicercatore' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('ricercatore.pubblicazioniRicercatore') }}">
+          <i class="material-icons">library_books</i>
+            <p>{{ __('Pubblicazioni scientifiche') }}</p>
+        </a>
+      </li>
+      <!--<li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('user.index') }}">
+          <i class="material-icons">monetization_on</i>
+            <p>{{ __('User Management') }}</p>
+        </a>
+      </li>-->
+        <!--<ul class="nav">
+            <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
+              <a class="nav-link" href="{{ route('profile.edit') }}">
+                <span class="sidebar-mini"> UP </span>
+                <span class="sidebar-normal">{{ __('User profile') }} </span>
+              </a>
+            </li>
+            <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
+              <a class="nav-link" href="{{ route('user.index') }}">
+                <span class="sidebar-mini"> BU </span>
+                <span class="sidebar-normal"> {{ __('User Management') }} </span>
+              </a>
+            </li>
+          </ul>-->
+      <li class="nav-item{{ $activePage == 'budgetRicercatore' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('budgetRicercatore') }}">
+          <i class="material-icons">monetization_on</i>
+            <p>{{ __('Richieste Budget') }}</p>
+        </a>
+      </li>
+      <!--<li class="nav-item{{ $activePage == 'typography' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('typography') }}">
+          <i class="material-icons">library_books</i>
+            <p>{{ __('Typography') }}</p>
+        </a>
+      </li>-->
+      <!--<li class="nav-item{{ $activePage == 'icons' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('icons') }}">
+          <i class="material-icons">bubble_chart</i>
+          <p>{{ __('Icons') }}</p>
+        </a>
+      </li>-->
+      <!--<li class="nav-item{{ $activePage == 'map' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('map') }}">
+          <i class="material-icons">location_ons</i>
+            <p>{{ __('Maps') }}</p>
+        </a>
+      </li>-->
+      <!--<li class="nav-item{{ $activePage == 'notifications' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('notifications') }}">
+          <i class="material-icons">notifications</i>
+          <p>{{ __('Notifications') }}</p>
+        </a>
+      </li>-->
+      <!--<li class="nav-item active-pro{{ $activePage == 'upgrade' ? ' active' : '' }}">
+        <a class="nav-link text-white bg-danger" href="{{ route('upgrade') }}">
+          <i class="material-icons text-white">unarchive</i>
+          <p>{{ __('Upgrade to PRO') }}</p>
+        </a>
+      </li>-->
+    </ul>
+  </div>
+</div>
