2f2b084ab09a0c37dab85c3b5765b754dbba596c
giovannispaziani <giovanni.spaziani@student.univaq.it>
2022-06-14 14:54:34 +0200

    test registrazione

--- tests/Feature/projectDashboardTest.php
+++ tests/Feature/projectDashboardTest.php
@@ -6,17 +6,17 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class projectDashboardTest extends TestCase
-{
+/**class projectDashboardTest extends TestCase
+{**/
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_()
+    /**public function test_()
     {
         $response = $this->get('/project-dashboard');
 
         $response->assertStatus(200);
     }
-}
+}**/
