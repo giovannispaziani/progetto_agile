89669136e1bfd400ef5b2986ad80767741607d65
maca99 <matteo.cavasinni@student.univaq.it>
2022-06-27 10:26:58 +0200

    fix sidebar

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,42 +1,23 @@
 <div class="sidebar" data-color="orange" data-background-color="white" data-image="{{ asset('material') }}/img/sidebar-1.jpg">
-    @switch(Auth::user()->type)
-      @case("Ricercatore")
-      <div class="logo">
-        <a href="http://127.0.0.1:8000/ricercatore" class="simple-text logo-normal">
-          {{ __('Stark Industries') }}
+@switch(Auth::user()->type)
+  @case("Ricercatore")
+  <div class="logo">
+    <a href="http://127.0.0.1:8000/ricercatore" class="simple-text logo-normal">
+      {{ __('Stark Industries') }}
+    </a>
+  </div>
+  <div class="sidebar-wrapper">
+    <ul class="nav">
+      <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('home') }}">
+          <i class="material-icons">dashboard</i>
+            <p>{{ __('Dashboard') }}</p>
         </a>
-      </div>
-      <div class="sidebar-wrapper">
-        <ul class="nav">
-          <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
-            <a class="nav-link" href="{{ route('home') }}">
-              <i class="material-icons">dashboard</i>
-                <p>{{ __('Dashboard') }}</p>
-            </a>
-          </li>
-          <li class="nav-item{{ $activePage == 'Richieste Budget' ? ' active' : '' }}">
-            <a class="nav-link" href="{{ route('budgetRicercatore') }}">
-              <i class="material-icons">monetization_on</i>
-                <p>{{ __('Richieste Budget') }}</p>
-            </a>
-          </li>
-          <li class="nav-item{{ $activePage == 'Pubblicazioni' ? ' active' : '' }}">
-            <a class="nav-link" href="{{ route('home') }}">
-              <i class="material-icons">library_books</i>
-                <p>{{ __('Pubblicazioni') }}</p>
-            </a>
-          </li>
-          <li class="nav-item{{ $activePage == 'Dashboard' ? ' active' : '' }}">
-            <a class="nav-link" href="{{ route('pubblicazioniScientifiche') }}">
-              <i class="material-icons">library_books</i>
-                <p>{{ __('Pubblicazioni scientifiche') }}</p>
-        </ul>
-      </div>
-      @break
-      @case("Manager")
-      <div class="logo">
-        <a href="http://127.0.0.1:8000/ricercatore" class="simple-text logo-normal">
-          {{ __('Stark Industries') }}
+      </li>
+      <li class="nav-item{{ $activePage == 'profileRicercatore' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('home') }}">
+          <i class="material-icons">library_books</i>
+            <p>{{ __('Pubblicazioni esterne') }}</p>
         </a>
       </li>
       <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
@@ -68,16 +49,17 @@
           <p>{{ __('Registrazione Finanziatore') }}</p>
         </a>
       </li>
-      <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('project-list') }}">
-        <i class="material-icons">work</i>
-          <p>{{ __('Lista Progetti')}}</p>
-        </a>
-      </li>
+
       <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
         <a class="nav-link" href="{{ route('project-create') }}">
         <i class="material-icons">work</i>
-          <p>{{ __('Creazione progetto')}}</p>
+          <p>{{ __('Creazione progetto ')}}</p>
+        </a>
+      </li>
+      <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('project-list') }}">
+        <i class="material-icons">work</i>
+          <p>{{ __('Lista progetti')}}</p>
         </a>
       </li>
     </ul>
@@ -95,21 +77,11 @@
         <a class="nav-link" href="{{ route('home') }}">
           <i class="material-icons">dashboard</i>
             <p>{{ __('Dashboard') }}</p>
-
-
         </a>
-      </div>
-      <div class="sidebar-wrapper">
-        <ul class="nav">
-          <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
-            <a class="nav-link" href="{{ route('home') }}">
-              <i class="material-icons">dashboard</i>
-                <p>{{ __('Dashboard') }}</p>
-            </a>
-          </li>
-        </ul>
-      </div>
-      @break
-    @endswitch
+      </li>
+    </ul>
+  </div>
+  @break
+@endswitch
 
-    </div>
+</div>
