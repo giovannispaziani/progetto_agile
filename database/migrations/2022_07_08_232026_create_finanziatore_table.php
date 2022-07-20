1702fc23c45774318406e171f9fd5dd0b514b6c9
giovannispaziani <giovanni.spaziani@student.univaq.it>
2022-07-21 00:09:28 +0200

    Fix tabella finanziatore nel db

--- database/migrations/2022_07_08_232026_create_finanziatore_table.php
+++ database/migrations/2022_07_08_232026_create_finanziatore_table.php
@@ -13,12 +13,21 @@ class CreateFinanziatoreTable extends Migration
      */
     public function up()
     {
+        
         Schema::create('finanziatore', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('id_progetto');
             $table->unsignedBigInteger('id_finanziatore');
             $table->decimal('fondo');
+            $table->timestamps();
         });
+
+        Schema::table('finanziatore', function (Blueprint $table) {
+            $table->foreign('id_progetto')->references('id')->on('projects')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('id_finanziatore')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+
+
     }
 
     /**
