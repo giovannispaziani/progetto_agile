c1acaca14093ee2ea601a36da317160f73d9bc51
Emanuele Scarsella <emascars@gmail.com>
2022-07-25 23:23:33 +0200

    correzione titolo in projectList

--- resources/views/pages/projectList.blade.php
+++ resources/views/pages/projectList.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['activePage' => 'dashboard', 'titlePage' => __('Dashboard progetto')])
+@extends('layouts.app', ['activePage' => 'dashboard', 'titlePage' => __('Lista progetti')])
 
 @section('content')
 <div class="content">
